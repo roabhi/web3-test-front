fa7fb3209c87aeab497fb9a1a3dd944b3e5aa834
roabhi <teskostudio@gmail.com>
2022-04-20 18:22:32 +0200

    sendTransaction wip

--- src/js/utils/web3.utils.js
+++ src/js/utils/web3.utils.js
@@ -5,12 +5,23 @@ import { addressBalance, CONTRACT_ADDRESS, TOKEN_PRICE } from "../globals/dom.gl
 const web3 = new Web3(new Web3.providers.HttpProvider("https://rinkeby.infura.io/v3/9187e875d2104604b97f1a00fe8783b7"))
 const contract = new web3.eth.Contract(abi,CONTRACT_ADDRESS)
 
-export const mintTokens = (address, amount) => {
-    const price = web3.utils.toWei(TOKEN_PRICE),
-            quantity = web3.utils.toBN(String(amount))
-    console.log(price, quantity)
+export const mintTokens = async(address, amount) => {
     
-    const res = await contract.methods.Mint(price,quantity).send({from: address})
+    if (amount && amount > 0)
+    {
+        const price = web3.utils.toWei(TOKEN_PRICE),
+            quantity = web3.utils.toBN(String(amount))
+
+        // const res = await contract.methods.Mint(quantity).send({from: address})
+
+        const contractAbi = eth.contract(abi)
+        const myContract = contractAbi.at(CONTRACT_ADDRESS)
+        const getData = myContract.Mint.getData(price, quantity)
+
+        const res = await web3.eth.sendTransaction({to:CONTRACT_ADDRESS, from:address, data:getData});
+    }
+    
+    
 },
 
 getContractOwner = async (address) =>{
