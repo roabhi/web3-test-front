22e4a992ac1655b61c7d37372792567a079de9b6
roabhi <teskostudio@gmail.com>
2022-10-26 18:49:16 +0200

    Updated code for re-deploy on Goerli

--- src/js/globals/dom.globals.js
+++ src/js/globals/dom.globals.js
@@ -1,16 +1,16 @@
- /* ================================
+/* ================================
     Global vars
 ====================================*/
 
 export const connectBtn = document.getElementById('access'),
-      sendBtn = document.getElementById('send'),
-      amountCounter = document.getElementById('amount-counter'),
-      addressDisplay = document.getElementById('user-wallet-address'),
-      addressBalance = document.getElementById('user-balance'),
-      contractUi = document.getElementById('contract-ui'),
-      mintingFee = document.getElementById('fee'),
-      tokenSupply = document.getElementById('supply'),
-      alertMsg = document.getElementById('alert-msg'),
-      NET = 'rinkeby',
-      CONTRACT_ADDRESS = '0xA8358be35294C67403E2bd53A1b958F6188CeB3f',
-      TOKEN_PRICE = '0.05'
+  sendBtn = document.getElementById('send'),
+  amountCounter = document.getElementById('amount-counter'),
+  addressDisplay = document.getElementById('user-wallet-address'),
+  addressBalance = document.getElementById('user-balance'),
+  contractUi = document.getElementById('contract-ui'),
+  mintingFee = document.getElementById('fee'),
+  tokenSupply = document.getElementById('supply'),
+  alertMsg = document.getElementById('alert-msg'),
+  NET = 'goerli',
+  CONTRACT_ADDRESS = '0x92EbA29b65441E64A21B4ed2d7deB91E72B6EbfF',
+  TOKEN_PRICE = '0.005'
--- src/js/index.js
+++ src/js/index.js
@@ -1,126 +1,113 @@
 /**
- * REMIX IDE CONTRACT URL -> https://remix.ethereum.org/#optimize=true&runs=200&evmVersion=null&version=soljson-v0.8.4+commit.c7e474f2.js
+ * GOERLI ETHERSCAN CONTRACT ADDRESS -> 0x92EbA29b65441E64A21B4ed2d7deB91E72B6EbfF
  */
 
-
 import 'regenerator-runtime/runtime'
-import { getBalanceFromAddress, getContractOwner, mintTokens, getTokenSupply, getAccounts } from './utils/ethers.utils'
-import { connectBtn, sendBtn, addressDisplay, addressBalance, amountCounter, contractUi, NET, tokenSupply, mintingFee, TOKEN_PRICE } from './globals/dom.globals'
-     
+import {
+  getBalanceFromAddress,
+  getContractOwner,
+  mintTokens,
+  getTokenSupply,
+  getAccounts,
+} from './utils/ethers.utils'
+import {
+  connectBtn,
+  sendBtn,
+  addressDisplay,
+  addressBalance,
+  amountCounter,
+  contractUi,
+  NET,
+  tokenSupply,
+  mintingFee,
+  TOKEN_PRICE,
+} from './globals/dom.globals'
 
 const onAmountChange = (e) => {
-    
-    
     let currentVal = amountCounter.value
-    
-    if (e.target.classList.contains('increase'))
-    {
-        if (currentVal < 10 )
-            currentVal++
-    }
-    else
-    {
-        if (currentVal > 0)        
-            currentVal--        
+
+    if (e.target.classList.contains('increase')) {
+      if (currentVal < 10) currentVal++
+    } else {
+      if (currentVal > 0) currentVal--
     }
 
     amountCounter.value = currentVal
-},
-
-
-broadcast = (e) => {
+  },
+  broadcast = (e) => {
     mintTokens(localStorage.getItem(`${NET}`), amountCounter.value)
-    .then(() => {
-        getTokenSupply()
-        .then((res) => tokenSupply.innerText = `Minted MNTs : ${res.toString()}`)
-    })
-    .then(() => {
-        const accounts = getAccounts()
-        .then((accounts) => {
-            getBalanceFromAddress(accounts[0])
-            .then((res) => addressBalance.innerText = res.substring(0,6) + ' ETH')})
-    })
-
-    
-}, 
-
-displayData = (address) =>
-{
+      .then(() => {
+        getTokenSupply().then(
+          (res) => (tokenSupply.innerText = `Minted MNTs : ${res.toString()}`)
+        )
+      })
+      .then(() => {
+        const accounts = getAccounts().then((accounts) => {
+          getBalanceFromAddress(accounts[0]).then(
+            (res) => (addressBalance.innerText = res.substring(0, 6) + ' ETH')
+          )
+        })
+      })
+  },
+  displayData = (address) => {
     document.body.classList.add('logged')
-    connectBtn.innerText = "disconnect"
-    addressDisplay.innerText = '' + address.substr(0,6) + '...' + address.substr(address.length - 3)
+    connectBtn.innerText = 'disconnect'
+    addressDisplay.innerText =
+      '' + address.substr(0, 6) + '...' + address.substr(address.length - 3)
     addressDisplay.setAttribute('title', address)
     mintingFee.innerText = TOKEN_PRICE
-    getBalanceFromAddress(address)
-    .then((res) => addressBalance.innerText = res.substring(0,6) + ' ETH')
-    getTokenSupply()
-    .then((res) => tokenSupply.innerText = `Minted MNTs : ${res.toString()}`)
-    getContractOwner(address)
-    .then((res) => console.log('owner is ', res))
+    getBalanceFromAddress(address).then(
+      (res) => (addressBalance.innerText = res.substring(0, 6) + ' ETH')
+    )
+    getTokenSupply().then(
+      (res) => (tokenSupply.innerText = `Minted MNTs : ${res.toString()}`)
+    )
+    getContractOwner(address).then((res) => console.log('owner is ', res))
     contractUi.classList.remove('hidden')
-
-},
-
-metaLogIn = async() => {
-    
-    const accounts =  getAccounts().then((accounts) => {
-        if (!document.body.classList.contains('logged'))    {
-        
-            localStorage.setItem(`${NET}`, accounts[0])
-            displayData(accounts[0])
-        }
-        else 
-        {
-            document.body.classList.remove('logged')
-            connectBtn.innerText = "connect"
-            addressDisplay.innerText = 'user address'
-            addressDisplay.setAttribute('title', '')
-            addressBalance.innerText = "ETH"
-            tokenSupply.innerText = ''
-            localStorage.removeItem(`${NET}`)
-            contractUi.classList.add('hidden')
-    
-        }
+  },
+  metaLogIn = async () => {
+    const accounts = getAccounts().then((accounts) => {
+      if (!document.body.classList.contains('logged')) {
+        localStorage.setItem(`${NET}`, accounts[0])
+        displayData(accounts[0])
+      } else {
+        document.body.classList.remove('logged')
+        connectBtn.innerText = 'connect'
+        addressDisplay.innerText = 'user address'
+        addressDisplay.setAttribute('title', '')
+        addressBalance.innerText = 'ETH'
+        tokenSupply.innerText = ''
+        localStorage.removeItem(`${NET}`)
+        contractUi.classList.add('hidden')
+      }
     })
-    
-       
-    
-},
-
-init = (e) => {
+  },
+  init = (e) => {
     document.removeEventListener('DOMContentLoaded', init, false)
 
-    if (window.ethereum)
-    {
-        // console.log(window.ethereum.isConnected())
-        if (localStorage.getItem(`${NET}`) && window.ethereum.isConnected())
-        {
-            displayData(localStorage.getItem(`${NET}`))
-        }
-        else
-        {
-            connectBtn.innerText = "connect"
-            // connectBtn.addEventListener('click', metaLogIn, false)
-        }
+    if (window.ethereum) {
+      // console.log(window.ethereum.isConnected())
+      if (localStorage.getItem(`${NET}`) && window.ethereum.isConnected()) {
+        displayData(localStorage.getItem(`${NET}`))
+      } else {
+        connectBtn.innerText = 'connect'
+        // connectBtn.addEventListener('click', metaLogIn, false)
+      }
 
-        connectBtn.addEventListener('click', metaLogIn, false)
-        sendBtn.addEventListener('click', broadcast, false)
-        Array.from(document.querySelectorAll("button.amount")).map((obj) => {
-            obj.addEventListener('click', onAmountChange, false)
-        })
-
-        
-        
+      connectBtn.addEventListener('click', metaLogIn, false)
+      sendBtn.addEventListener('click', broadcast, false)
+      Array.from(document.querySelectorAll('button.amount')).map((obj) => {
+        obj.addEventListener('click', onAmountChange, false)
+      })
+    } else {
+      connectBtn.innerText = 'install metamask'
+      connectBtn.classList.remove(
+        'bg-emerald-400',
+        'shadow-lg',
+        'shadow-emerald-400/50'
+      )
+      connectBtn.classList.add('bg-red-400')
     }
-    else 
-    {
-        connectBtn.innerText = "install metamask"
-        connectBtn.classList.remove('bg-emerald-400', 'shadow-lg', 'shadow-emerald-400/50')
-        connectBtn.classList.add('bg-red-400')
-    }
-
-}
-
-
+  }
 
 document.addEventListener('DOMContentLoaded', init, false)
